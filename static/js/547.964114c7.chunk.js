cc8990bb48b052c8399cce13a6db7edae5815ea6
Piotr Maksymiuk <kiszuriwalilibori@poczta.onet.pl>
2022-12-29 13:33:19 +0100

    Updates

---
+++ static/js/547.964114c7.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkunsplash=self.webpackChunkunsplash||[]).push([[547],{7547:function(e,n,s){s.r(n),s.d(n,{default:function(){return c}});var u=s(2791),o=s(7689),t=s(184),c=function(){var e=(0,o.s0)();return(0,u.useEffect)((function(){setTimeout((function(){e(-1)}),2e3)}),[]),(0,t.jsxs)("div",{className:"nopage",children:[(0,t.jsxs)("h2",{children:['Ojejku! Strony o adresie "',decodeURIComponent(window.location.href),'" po prostu nie ma \ud83d\ude25']}),";"]})}}}]);
+//# sourceMappingURL=547.964114c7.chunk.js.map
