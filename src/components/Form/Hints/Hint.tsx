02bd68c52f3fb991dde933886b673e3e6fa6a4f0
Piotr Maksymiuk <kiszuriwalilibori@poczta.onet.pl>
2023-07-17 12:53:12 +0200

    hint buttons are available by tabs, image tags are without pointer now

--- src/components/Form/Hints/Hint.tsx
+++ src/components/Form/Hints/Hint.tsx
@@ -2,42 +2,30 @@ import * as React from "react";
 import debounce from "lodash/debounce";
 import { connect } from "react-redux";
 
-
 import { AppDispatch } from "types";
 import { fetchImages } from "reduxware/redux/thunks";
 interface Props {
-  hint: string;
-  fetchImages:Function;
+    hint: string;
+    fetchImages: Function;
 }
 
 const Hint = (props: Props) => {
-  const { hint, fetchImages } = props;
+    const { hint, fetchImages } = props;
 
-const clickHandler = debounce(() => {  
-          fetchImages(hint);
-        }, 200);
+    const clickHandler = debounce(() => {
+        fetchImages(hint);
+    }, 200);
 
-  if (!hint) return null;
-  return  (
-      <div className="images-hint" onClick = {clickHandler}>
-      {hint}
-    </div>
-  );
-  }
+    if (!hint) return null;
+    return (
+        <button className="images-hint" onClick={clickHandler} tabIndex={0}>
+            {hint}
+        </button>
+    );
+};
 
-const mapDispatchToProps = (dispatch:AppDispatch) => ({
-  fetchImages: (str:string) => dispatch(fetchImages(str)),
-  
+const mapDispatchToProps = (dispatch: AppDispatch) => ({
+    fetchImages: (str: string) => dispatch(fetchImages(str)),
 });
 
 export default connect(null, mapDispatchToProps)(Hint);
-
-
-
-
-
-
-
-
-
-
